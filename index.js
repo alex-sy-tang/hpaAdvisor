8bed1bebc09c6c4cb35f8471b33f6f8297bfb6e8
SHENGYAO Tang <shengyaotang@SHENGYAOs-MacBook-Air.local>
2022-03-09 22:06:19 -1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(window).scroll(function(event){
+    if($(document).scrollTop()>50){
+      $(".navbar").removeClass('navbar-dark').addClass('navbar-light');
+      $(".navbar").removeClass('bg-dark').addClass('bg-light');
+    }else{
+      $(".navbar").removeClass('navbar-light').addClass('navbar-dark');
+      $(".navbar").removeClass('bg-light').addClass('bg-dark');
+    }
+  })
+})
