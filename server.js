2e9e340a3c7b7d92f98cc8d9548dd9683c56c031
alex.sy.tang <alex.sy.tang@gmail.com>
2022-03-28 11:53:38 -1000

    Set up the ejs files

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.set('view engine','ejs')
+
 
 app.listen('3000',()=>{
   console.log('The server is set up and running on port 3000')
 })
 
 app.get('/',(req,res)=>{
-  res.sendFile('index.html')
+  res.render('index')
 })
 
 app.get('/food',(req,res)=>{
-  res.sendFile('food.html')
+  res.render('food')
 })
 
 app.get('/trip',(req,res)=>{
-  res.sendFile('trip.html')
+  res.render('trip')
 })
 
 app.get('/yours',(req,res)=>{
-  res.sendFile('yours.html')
+  res.render('yours')
 })
